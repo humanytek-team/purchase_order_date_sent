403a9d4c6817121b0dca9fcf327247313584dce0
Manuel Marquez <buzondemam@gmail.com>
2017-08-26 00:58:40 -0400

    [ADD] Add note of changes in file CHANGELOG, also adds translation of new terms added by the addon.

--- __openerp__.py
+++ __openerp__.py
@@ -23,7 +23,7 @@
 {
     'name': "Record the date of sent of quotation request to supplier",
     'description': """
-        Adds field "Date of sent of quotation request" to model of purchase
+        Adds field "Date sent to supplier" to model of purchase
         orders for allow record the date in which the quotation request is sent
         to supplier. This date is initially registered by the module
         automatically but the user can modify it.
